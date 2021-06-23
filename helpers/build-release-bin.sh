e20a6237d24c14df64e352e0eb201cc327029816
Anton Pyrogovskyi <anton@wireleap.com>
2021-06-23 18:51:33 +0300

    add conditional release build step

---
+++ helpers/build-release-bin.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -e
+
+SCRIPT_NAME="$(basename "$0")"
+
+fatal() { echo "FATAL [$SCRIPT_NAME]: $*" 1>&2; exit 1; }
+info() { echo "INFO [$SCRIPT_NAME]: $*"; }
+
+usage() {
+cat<<EOF
+Syntax: $SCRIPT_NAME /path/to/outdir linux|darwin|all
+Helper script to compile components (inside docker) and build release dist binaries
+
+EOF
+exit 1
+}
+
+[ -n "$1" ] || usage
+
+command -v docker >/dev/null || fatal "docker not installed"
+
+SRCDIR="$(realpath "$1")"
+OUTDIR="$(realpath "$2")"
+
+case $2 in
+    linux|darwin)   TARGETS="$2";;
+    all)            TARGETS="linux darwin";;
+    *)              fatal "target_os not specified or supported: $2";;
+esac
+
+for target_os in $TARGETS; do
+    info "building for: $target_os"
+    name=$target_os-amd64
+    mkdir -p "$OUTDIR/.deps" "$OUTDIR/$name"
+    DEPS_CACHE="$OUTDIR/.deps" TARGET_OS="$target_os" BUILD_TAGS="upgrade" \
+        "$SRCDIR/contrib/docker/build-bin.sh" "$OUTDIR/$name"
+
+    cd "$OUTDIR/$name"
+
+    version="$("$SRCDIR"/contrib/gitversion.sh)"
+    for bin in *; do
+        bname="$(basename "$bin")"
+        minor="$(echo "$version" | cut -d'.' -f1-2)"
+        out="$bname/$version"
+        mkdir -p "$out/changelogs"
+        echo "$version" > "$out/version.txt"
+        "$SRCDIR/.github/helpers/gen-signature.sh" "$bin"
+        mv "$bin" "$bin.hash" "$out"
+        cp "$SRCDIR/changelogs/$minor/$bname.md" "$out/changelog.md"
+        ln -nsf "$out" "$bname/latest"
+    done
+done
+
---
+++ helpers/gen-signature.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+set -e
+
+fatal() { echo "FATAL [$(basename $0)]: $@" 1>&2; exit 1; }
+info() { echo "INFO [$(basename $0)]: $@"; }
+
+usage() {
+cat<<EOF
+Syntax: $0 filepath
+Generate hash file for filepath and check if it verifies
+
+Arguments:
+
+    filepath        - path to file to be signed
+
+EOF
+exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+fi
+
+filepath=$1
+filename=$(basename $1)
+
+info "generating sha256sum"
+SHA256SUM="$(sha256sum $filepath | cut -d " " -f 1)  $filename"
+
+info "generating sha512sum"
+SHA512SUM="$(sha512sum $filepath | cut -d " " -f 1)  $filename"
+
+info "writing $filepath.hash"
+cat > $filepath.hash <<EOF
+To ensure the binary has not been corrupted in transit or tampered with,
+perform the following two steps to cryptographically verify binary integrity:
+
+1. Verify the authenticity of this file by checking that it is signed with our
+   GPG release key (the signature date might differ but the fingerprint must
+   be identical to the one specified here):
+
+    $ gpg --recv-keys --keyserver pool.sks-keyservers.net 693C86E9DECA9D07D79FF9D22ECD72AD056012E1
+    $ gpg --list-keys --with-fingerprint builds@wireleap.com
+      pub   rsa4096 2021-03-26 [SC]
+            693C 86E9 DECA 9D07 D79F  F9D2 2ECD 72AD 0560 12E1
+      uid           [unknown] Wireleap CI Automated Build Signing Key <builds@wireleap.com>
+      sub   rsa4096 2021-03-26 [E]
+    $ gpg --verify $filename.hash
+      gpg: Signature made Fri Mar 26 13:38:53 2021 UTC
+      gpg:                using RSA key 693C86E9DECA9D07D79FF9D22ECD72AD056012E1
+      gpg: Good signature from "Wireleap CI Automated Build Signing Key <builds@wireleap.com>" [unknown]
+      gpg: WARNING: This key is not certified with a trusted signature!
+      gpg:          There is no indication that the signature belongs to the owner.
+      Primary key fingerprint: 693C 86E9 DECA 9D07 D79F  F9D2 2ECD 72AD 0560 12E1
+
+2. Recalculate the binary hash and make sure it matches your choice of hash below.
+
+    $ sha256sum $filename
+      $SHA256SUM
+
+    $ sha512sum $filename
+      $SHA512SUM
+
+   Note, you can compare hashes automatically::
+
+    $ sha256sum -c $filename.hash
+      $filename: OK
+
+    $ sha512sum -c $filename.hash
+      $filename: OK
+
+EOF
+
+info "signing $filepath.hash"
+gpg --local-user 693C86E9DECA9D07D79FF9D22ECD72AD056012E1 --output "$filepath.hash.asc" --clearsign "$filepath.hash"
+mv "$filepath.hash.asc" "$filepath.hash"
+
+info "verifying gpg signature"
+gpg --verify --batch "$filepath.hash"
+
+info "verifying checksum"
+
+# for sha*sum to work as it looks in . for the file to check
+dir="$(dirname "$filepath")"
+
+if [ "$dir" != '.' ]; then
+    cd "$dir"
+    sha256sum -c "$(basename "$filepath").hash"
+    sha512sum -c "$(basename "$filepath").hash"
+else
+    sha256sum -c "$filepath.hash"
+    sha512sum -c "$filepath.hash"
+fi
+
+info "signed and verified successfully"
